5180b0df90540967517ee6c685ab0fce02cd5edc
PaperBag <ben.zichri@gmail.com>
2018-11-03 18:23:26 +0200

    forgot webm can also be a video file

--- youtube.py
+++ youtube.py
@@ -2,7 +2,7 @@ import youtube_dl
 import os
 
 PLAYLIST_LEN = 452  # this kinda sucks
-PLAYLIST_URL = 'https://www.youtube.com/playlist?list=PLiWL8lZPZ2_k1JH6urJ_H7HzH9etwmn7M'
+PLAYLIST_URL = ('https://www.youtube.com/playlist?list=PLiWL8lZPZ2_k1JH6urJ_H7HzH9etwmn7M',)
 
 
 class Video(object):
@@ -12,19 +12,19 @@ class Video(object):
 	:param only_audio: True to download only audio, False to download video and audio
 	"""
 	def __init__(self, i: int, only_audio: bool):
-		self.i = i
-		self.fmt = 'webm' if only_audio else 'mp4'
-	
-	def __enter__(self):
-		with youtube_dl.YoutubeDL({
-			'playlist_items': str(PLAYLIST_LEN - self.i),
+		self.fmt = 'bestaudio[ext=webm]' if only_audio else 'best[ext=mp4]'
+		self.opts = {
+			'playlist_items': str(PLAYLIST_LEN - i),
 			'format': self.fmt,
 			'outtmpl': self.fmt,
 			# These don't work
 			# 'keepvideo': False
 			# 'playlistreverse': True
-		}) as yt:
-			yt.download([PLAYLIST_URL])
+		}
+	
+	def __enter__(self):
+		with youtube_dl.YoutubeDL(self.opts) as yt:
+			yt.download(PLAYLIST_URL)
 		
 		self.f = open(self.fmt, 'rb')
 		return self.f
