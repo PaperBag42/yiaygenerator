cf8e1ffe0978b55fb3d0a9fa3bcd4b6c2af6ee90
PaperBag <ben.zichri@gmail.com>
2018-12-05 00:37:13 +0200

    split functions for async

--- stt.py
+++ stt.py
@@ -1,4 +1,4 @@
-from typing import Tuple, List
+from typing import Tuple, List, Dict, Iterable, Generator
 
 import youtube
 
@@ -9,6 +9,8 @@ import json
 import time
 import io
 from os import path, environ
+
+import logging
 from logging import info
 
 STT_PATH = 'expr/stt/'
@@ -18,62 +20,95 @@ service = watson.SpeechToTextV1(
 	username=environ['WATSON_USERNAME'],
 	password=environ['WATSON_PASSWORD']
 )
+# TODO: would be cool if I registered a callback here
 
 
-def speech_to_text(i: int) -> Tuple[str, List]:
+def speech_to_text(inds: Iterable[int]) -> Generator[Tuple[int, str, List]]:
 	"""
-	Loads the speech-to-text transcript for a YIAY video.
-	Makes an API request if a transcript was not found.
+	Loads speech-to-text transcripts for a list of YIAY videos.
+	Makes an API request for each transcript that was not found.
+
+	:param inds: the video's index in the playlist
+	:return: a tuple containing an index, a full transcript and word timestamps for each video.
+	"""
+	ids = {}
+	for i in inds:
+		filename = f'{STT_PATH}{i:03d}.json'
+		if path.isfile(filename):
+			info(f'Loading transcript for YIAY #{i:03d} from {filename}.')
+			with open(filename) as file:
+				data = json.load(file)
+				yield i, data['transcript'], data['timestamps']
+		else:
+			with youtube.video(i, only_audio=True) as file:
+				ids[request(file, i)] = i
+	
+	yield from resolve(ids)
+
+
+def request(stream: io.BufferedReader, i: int) -> str:
+	"""
+	Sends an audio file to the speech-to-text API.
+
+	:param stream: audio file binary data
+	:param i: the video's index in the playlist
+	:return: the created job's ID.
+	"""
+	try:
+		info(f'Making an API request for YIAY #{i:03d}...')
+		return service.create_job(
+				audio=stream,
+				content_type='audio/webm',
+				language_customization_id=environ.get('WATSON_CUSTOMIZATION_ID'),  # costs money
+				timestamps=True,
+				profanity_filter=False
+			).get_result()['id']
+		
+	except watson.WatsonApiException:  # TODO: get response from support
+		logging.warning(f'YIAY #{i:03d}: Got the weird error again, retrying...')
+		stream.seek(0)
+		time.sleep(5)
+		
+		return request(stream, i)
+
+
+def resolve(ids: Dict[int]) -> Generator[Tuple[int, str, List]]:
+	"""
+	Waits for created jobs to complete, and fetches their responses.
+	
+	:param ids: a dict mapping each job ID to the index of the video it is working on
+	:return: index, transcript and timestamps for each job resolved
+	"""
+	pass
+
+
+def process(i: int, response: Dict) -> Tuple[int, str, List]:
+	"""
+	Processes the response from a completed job.
+	Saves the relevant data in a JSON file.
 	
 	:param i: the video's index in the playlist
-	:return: a full transcript, and timestamps for each word
-	"""
-	filename = f'{STT_PATH}{i:03d}.json'
-	if path.isfile(filename):
-		info(f'Loading transcript for YIAY #{i:03d} from {filename}.')
-		with open(filename) as file:
-			data = json.load(file)
-			return data['transcript'], data['timestamps']
-	else:
-		with youtube.video(i, only_audio=True) as file:
-			info(f'Making an API request for YIAY #{i:03d}...')
-			return request(file, filename)
-
-
-def request(stream: io.BufferedReader, out: str) -> Tuple[str, List]:
-	"""
-	Sends an audio file to the speech-to-text API,
-	gets the results and saves them in a JSON file.
-	
-	:param stream: audio file binary data
-	:param out: filename to save the result in
-	:return:
-		The audio's complete transcript,
-		and timestamps for each word.
+	:param response: response from a completed job
+	:return: index, transcript and word timestamps
 	"""
 	transcript = ''
 	timestamps = []
 	
-	for result in service.recognize(
-			audio=stream,
-			content_type='audio/webm',
-			language_customization_id=environ.get('WATSON_CUSTOMIZATION_ID'),  # costs money
-			timestamps=True,
-			profanity_filter=False
-	).get_result()['results']:
+	for result in response['results']:
 		alternative = result['alternatives'][0]  # only one alternative by default
 		
 		transcript += alternative['transcript']
 		timestamps.extend(alternative['timestamps'])
 	
 	# save to file
-	with open(out, 'w') as file:
+	with open(f'{STT_PATH}{i:03d}.json', 'w') as file:
 		json.dump({
+			# 'i': i,
 			'transcript': transcript,
 			'timestamps': timestamps
 		}, file, indent='\t')
 	
-	return transcript, timestamps
+	return i, transcript, timestamps
 
 
 # -- Customization --
