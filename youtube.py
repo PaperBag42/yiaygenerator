b72e9c600e300f452a5305c1956ead695857936e
PaperBag <ben.zichri@gmail.com>
2018-12-08 17:47:03 +0200

    use youtube_dl fork

--- youtube.py
+++ youtube.py
@@ -7,7 +7,6 @@ import os
 import io
 from logging import info
 
-PLAYLIST_LEN = 453  # this kinda sucks
 PLAYLIST_URL = ('https://www.youtube.com/playlist?list=PLiWL8lZPZ2_k1JH6urJ_H7HzH9etwmn7M',)
 
 
@@ -19,22 +18,25 @@ def video(i: int, only_audio: bool) -> ContextManager[io.BufferedReader]:
 	:param i: the video's index in the playlist (counting from 0)
 	:param only_audio: True to download only audio, False to download video and audio
 	:return: a context manager which returns the video file and removes it on exit.
+	:raise IndexError: if i surpasses the playlist's bounds
 	"""
 	fmt = 'bestaudio[ext=webm]' if only_audio else 'best[ext=mp4]'
 	
 	info(f'Downloading YIAY #{i:03d} as {"WEBM" if only_audio else "MP4"}...')
 	with youtube_dl.YoutubeDL({
-			'playlist_items': str(PLAYLIST_LEN + 1 - i),
+			'playlistreverse': True,
+			'playlist_items': str(i),
 			'format': fmt,
 			'outtmpl': fmt,
-			'quiet': True
-			# These don't work
-			# 'keepvideo': False
-			# 'playlistreverse': True
+			'quiet': True,
 		}) as yt:
 		yt.download(PLAYLIST_URL)
 	
-	file = open(fmt, 'rb')
+	try:
+		file = open(fmt, 'rb')
+	except FileNotFoundError:
+		raise IndexError
+	
 	try:
 		yield file
 	finally:
