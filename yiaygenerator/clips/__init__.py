7af0d590cef4333965273f0b42d037178bdba4b7
PaperBag <ben.zichri@gmail.com>
2018-12-22 19:57:05 +0200

    setup init file

--- yiaygenerator/clips/__init__.py
+++ yiaygenerator/clips/__init__.py
@@ -1,4 +1,12 @@
 """
 Generates clips from existing YIAY videos
 to be used as material for the YIAY generator.
+
+What it does:
+	- Downloads the video from Youtube
+	- Sends the audio to IBM's Watson Developer Cloud API
+	- Puts the results through a RegEx pattern to detect the important parts
+	- splits the video to clips and writes them to files
 """
+
+from .parser import create, CLIPS_PATH
