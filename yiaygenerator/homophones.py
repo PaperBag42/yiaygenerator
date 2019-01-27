bb90d2087f2c1ca84dbfbbd01a8edb08cea0f779
PaperBag <ben.zichri@gmail.com>
2019-01-27 22:32:33 +0200

    use cache for homophones

--- yiaygenerator/homophones.py
+++ yiaygenerator/homophones.py
@@ -1,7 +1,10 @@
 """
-Provides a list of homophones to let the same video clips be used as different words,
+Provides a list of homophones (words that sound like other words)
+to let the same video clips be used as different words,
 thus allowing broader usage of the video clips.
 """
+
+from django.core.cache import cache
 
 import re
 
@@ -9,24 +12,30 @@ HOMOPHONES_PATH = 'homophone/homophone_list.csv'
 
 _non_letter = re.compile(r'[^a-z]')
 
-_homophones = {}  # words that sound like other words
 
-# parses homophone_list.csv
-with open(HOMOPHONES_PATH) as file:
-	i = -1
-	homophone = ''
-	
-	next(file)
-	for line in file:
-		_, _, word, ind = line.split(',')
-		word = _non_letter.sub('', word.lower())
-		ind = int(ind)
+def load() -> None:
+	"""
+	Loads the homophones list from a CSV file
+	I stole from homophone.com.
+	"""
+	with open(HOMOPHONES_PATH) as file:
+		homophones = {}
+		i = -1
+		homophone = ''
 		
-		if i != ind:
-			i = ind
-			homophone = word
-		else:
-			_homophones[word] = homophone
+		next(file)
+		for line in file:
+			_, _, word, ind = line.split(',')
+			word = _non_letter.sub('', word.lower())
+			ind = int(ind)
+			
+			if i != ind:
+				i = ind
+				homophone = word
+			else:
+				homophones[word] = homophone
+	
+	cache.set('homophones', homophones)
 
 
 def get(word: str) -> str:
@@ -39,4 +48,4 @@ def get(word: str) -> str:
 		or the given word if no homophone was found.
 	"""
 	word = _non_letter.sub('', word.lower())
-	return _homophones.get(word, word)
+	return cache.get('homophones').get(word, word)
