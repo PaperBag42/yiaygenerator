4d3e2dd527a07f4846ef904d2ab7b83ab7faa4f9
PaperBag <ben.zichri@gmail.com>
2019-02-22 16:50:35 +0200

    render tweets with wkhtmltopdf

--- yiaygenerator/core/twitter.py
+++ yiaygenerator/core/twitter.py
@@ -1,14 +1,19 @@
 """
 Collects twitter posts to use as YIAY answers.
 """
-from typing import Generator, Dict, List
+from typing import Generator, Dict, List, ContextManager
 
 import twitter
+import django.template.loader
+import imgkit
 
-from os import environ
+import contextlib
+import tempfile
+import pathlib
+from os import environ, PathLike
 
 
-def tweets(hashtag: str) -> Generator[None]:
+def tweets(hashtag: str) -> Generator[PathLike]:
 	"""
 	Gets and processes Twitter posts to be used as YIAY answers.
 	:param hashtag: a Twitter hashtag to find tweets by
@@ -17,33 +22,59 @@ def tweets(hashtag: str) -> Generator[None]:
 	if not hashtag.startswith('#'):
 		hashtag = f'#{hashtag}'
 	
-	client = twitter.Twitter(
+	search = twitter.Twitter(
 		auth=twitter.OAuth2(bearer_token=environ['TWITTER_TOKEN']),
 		retry=True,
-	)
+	).search.tweets
 	
-	max_id = None
-	while True:
-		res = client.search.tweets(
-			q=hashtag, max_id=max_id,
-			lang='en',
-			tweet_mode='extended',
-			include_entities=True,
-		)
-		
-		yield None
-		
-		if 'next_results' not in res['search_metadata']:
-			break
-		max_id = min(s['id'] for s in res['statuses'])
+	with contextlib.ExitStack() as images:
+		max_id = None
+		while True:
+			res = search(
+				q=hashtag, max_id=max_id,
+				lang='en',
+				tweet_mode='extended',
+				include_entities=True,
+			)
+			
+			yield from (images.enter_context(_image(tweet)) for tweet in res['statuses'])
+			
+			if 'next_results' not in res['search_metadata']:
+				break
+			max_id = min(s['id'] for s in res['statuses'])
 
 
 def _validate(tweet: Dict):
 	pass
 
 
-def _processed(data: List[Dict]):
-	pass
+_template = django.template.loader.get_template('yiaygenerator/_tweet.html')
+_OFFSET = 520
+_options = {
+	'selector': '#main',
+	'quality': 100,
+	'height': 720,
+	# FIXME
+	# I've been killing bugs from this shit for a week,
+	# and now it decided to move 520px to the left for some reason
+	# so I'll just move it to the right
+	'crop-x': _OFFSET,
+	'crop-w': _OFFSET,
+}
+
+
+@contextlib.contextmanager
+def _image(data: List[Dict]) -> ContextManager[PathLike]:
+	"""
+	Converts data from a tweet to an image of the tweet.
+	:param data: a tweet object from the Twitter API.
+	:return: path to the tweet image file.
+	"""
+	with tempfile.NamedTemporaryFile(suffix='.jpg') as file:
+		name = file.name
+		imgkit.from_string(_template.render(data), name)
+		
+		yield pathlib.Path(name)
 
 
 def get_token() -> str:
