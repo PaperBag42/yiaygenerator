69157fd56d91e3fe6883781b92bb184495d1c0c9
PaperBag <ben.zichri@gmail.com>
2019-02-22 13:49:26 +0200

    add wkhtmltopdf to requirements

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,15 @@ apt update
 apt install -y ffmpeg
 apt install -y imagemagick && sed -i 's/<policy domain="path" rights="none" pattern="@\*"\/>//g' /etc/ImageMagick-6/policy.xml
 apt install -y python3-pip && pip3 install -r /vagrant/requirements.txt
+
+# wkhtmltopdf
+set -e
+apt install -y qt5-default libqt5webkit5-dev libqt5xmlpatterns5-dev libqt5svg5-dev
+git clone https://github.com/PaperBag42/wkhtmltopdf.git --single-branch --branch 'feature/selector'
+cd wkhtmltopdf/
+qmake
+make
+make install
+ldconfig /lib/
+cd ..
+rm -vrf wkhtmltopdf/
